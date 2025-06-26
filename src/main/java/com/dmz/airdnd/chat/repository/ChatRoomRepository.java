bf6eff090d6ad53e475c9d74fa15c37ec451058d
jang-jinuk <dlsndifn98@gmail.com>
2025-06-26 15:22:38 +0900

    feat: 채팅방 메세지 조회 쿼리 추가

--- src/main/java/com/dmz/airdnd/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/dmz/airdnd/chat/repository/ChatRoomRepository.java
@@ -1,8 +1,26 @@
 package com.dmz.airdnd.chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dmz.airdnd.chat.domain.ChatRoom;
+import com.dmz.airdnd.chat.dto.ChatMessageResponse;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+	@Query("""
+		    SELECT new com.dmz.airdnd.chat.dto.ChatMessageResponse(
+		        cm.sender.loginId,
+		        cm.content,
+		cm.sentAt
+		    )
+		    FROM ChatMessage cm
+		    WHERE cm.chatRoom.id = :roomId
+		    ORDER BY cm.sentAt ASC
+		""")
+	List<ChatMessageResponse> findMessagesByChatRoomId(
+		@Param("roomId") Long roomId
+	);
 }

f42dfc23a88a3204137455cc6d2bbc3e4df10d59
2jiyong <jdragon2000@naver.com>
2025-06-26 14:20:48 +0900

    feat: stomp 로그 추가

--- src/main/java/com/dmz/airdnd/chat/WebSocketConfig.java
+++ src/main/java/com/dmz/airdnd/chat/WebSocketConfig.java
@@ -1,13 +1,19 @@
 package com.dmz.airdnd.chat;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
@@ -19,12 +25,33 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws/chat")
 			.setAllowedOrigins("*")
-			.addInterceptors(jwtHandshakeInterceptor)
-			.withSockJS();
+			.addInterceptors(jwtHandshakeInterceptor);
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				log.debug("📥 [Inbound STOMP 메시지] " + message);
+				return message;
+			}
+		});
+	}
+
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				log.debug("📤 [Outbound STOMP 메시지] " + message);
+				return message;
+			}
+		});
 	}
 }

da6a96cccb1c5d85ae903856481223fad8f71937
2jiyong <jdragon2000@naver.com>
2025-06-26 14:29:44 +0900

    refactor: onetomany 제거

--- src/main/java/com/dmz/airdnd/chat/domain/ChatRoom.java
+++ src/main/java/com/dmz/airdnd/chat/domain/ChatRoom.java
@@ -1,17 +1,13 @@
 package com.dmz.airdnd.chat.domain;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -33,7 +29,4 @@ public class ChatRoom {
 	@CreationTimestamp
 	@Column(updatable = false)
 	private LocalDateTime createdAt;
-
-	@OneToMany(mappedBy = "chat_room", cascade = CascadeType.ALL) // 간단 구현을 위해 onetomany로 일단 구현
-	private List<ChatMessage> messages = new ArrayList<>();
 }

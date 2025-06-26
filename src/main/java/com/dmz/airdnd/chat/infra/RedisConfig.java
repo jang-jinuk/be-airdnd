a5b6bb86fa81d14b37e8bbf92a17c6d4754d55da
2jiyong <jdragon2000@naver.com>
2025-06-26 14:53:33 +0900

    feat: redisTemplate 빈 생성 추가

--- src/main/java/com/dmz/airdnd/chat/infra/RedisConfig.java
+++ src/main/java/com/dmz/airdnd/chat/infra/RedisConfig.java
@@ -3,9 +3,11 @@ package com.dmz.airdnd.chat.infra;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -21,5 +23,19 @@ public class RedisConfig {
 	@Bean
 	public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
 		return new MessageListenerAdapter(subscriber, "onMessage");
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new StringRedisSerializer());
+
+		template.afterPropertiesSet();
+		return template;
 	}
 }
